d63a97c69b330bee9d83007d3e1918a64db55ccb
+ <+>
2021-11-23 10:18:30 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -2,68 +2,69 @@ console.log('%c HI', 'color: firebrick')
 
 
 window.addEventListener('DOMContentLoaded', (e) => {
-    getDogPics()
-    getDogBreeds()
-    dropdownEventListener()
-    // getDropdownBox()
+  getDogPics()
+  getDogBreeds()
+  dropdownEventListener()
+  // getDropdownBox()
 })
 
 
 //getting dogs
 
 function getDogPics() {
-    fetch("https://dog.ceo/api/breeds/image/random/4")
-        .then(response => response.json())
-        .then(dogPics => dogPics.message.forEach(dogImageURL => renderDogsToDOM(dogImageURL)))
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(dogPics => dogPics.message.forEach(dogImageURL => renderDogsToDOM(dogImageURL)))
 }
 
 function getDogBreeds() {
-    fetch('https://dog.ceo/api/breeds/list/all')
-        .then(response => response.json())
-        .then(dogBreeds => Object.keys(dogBreeds.message).forEach(breed => addDogBreeds(breed)))
+  fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(dogBreeds => Object.keys(dogBreeds.message).forEach(breed => addDogBreeds(breed)))
 }
 
 //Renderers
 function renderDogsToDOM(pic) {
-    const pictureDisplay = document.getElementById("dog-image-container")
-    const img = document.createElement('img')
-    img.src = pic
-    pictureDisplay.appendChild(img)
+  const pictureDisplay = document.getElementById("dog-image-container")
+  const img = document.createElement('img')
+  img.src = pic
+  pictureDisplay.appendChild(img)
 
 }
 
 function addDogBreeds(breed) {
-    //Get nodes
-    const dogBreedList = document.getElementById("dog-breeds")
-    const dropdownBox = document.getElementById("breed-dropdown")
-    
-    //create li, set text, add event listener, add to DOM
-    const li = document.createElement("li")
-    li.innerText = breed
-    li.addEventListener('click', (e) => li.style.color = "red")
-    dogBreedList.appendChild(li)
-    
+  //Get nodes
+  const dogBreedList = document.getElementById("dog-breeds")
+  const dropdownBox = document.getElementById("breed-dropdown")
 
-    //compare li's text to value of dropdown box. If different, remove from dom
-    
-
-    // if (dropdownBox.value !== li.innerText.startsWith(dropdownBox.value)) {
-    //     li.remove
-    // }
+  //create li, set text, add event listener, add to DOM
+  const li = document.createElement("li")
+  li.innerText = breed
+  li.addEventListener('click', (e) => li.style.color = "red")
+  dogBreedList.appendChild(li)
 }
 
 
-function dropdownEventListener(){
-    const dropdownBox = document.getElementById("breed-dropdown")
-    const dogList = document.getElementById('dog-breeds').childNodes
-    dropdownBox.addEventListener("change", (e) =>{
-        if(dropdownBox.value === "a"){
-            dogList.filter(breed =>{
-                console.log(breed.startsWith("a"))
-                return breed.startsWith("a") 
-            })
-        }
-    })
+function dropdownEventListener() {
+  //select dropdown box
+  const dropdownBox = document.getElementById("breed-dropdown")
+
+  //select list of dogs on DOM
+  const dogList = document.getElementById('dog-breeds').childNodes
+
+  //when the value in the dropdown box changes, iterate through the list of dog names on the DOM and compare that new value to the first letter of each dog breed name
+  dropdownBox.addEventListener("change", (e) => {
+    if (dropdownBox.value === "a") {
+      dogList.filter(breed => {
+        console.log(breed.startsWith("a"))
+        return breed.startsWith("a")
+      })
+    } else if (dropdownBox.value === "b") {
+      dogList.filter(breed => {
+        return breed.startsWith("b")
+      })
+    }
+  })
 }
 
 
